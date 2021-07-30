80434065d822fe686108df0cb178c370b8d40ce3
Zach LeClaire <zacharyjleclaire@gmail.com>
2021-07-30 12:43:09 -0700

    adding service account comments

--- driveUtils.py
+++ driveUtils.py
@@ -4,6 +4,8 @@ import json
 import time
 import re
 
+# add service account login stuff
+
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
